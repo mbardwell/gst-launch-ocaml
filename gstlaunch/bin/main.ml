14a9714bcfc91d0980d2b4b24deffc4cf58a1c55
mdbardwell <bardwell@ualberta.ca>
2021-02-14 20:37:01 -0500

    basic gst-launch functionality working

--- gstlaunch/bin/main.ml
+++ gstlaunch/bin/main.ml
@@ -1,15 +1,23 @@
 open Core
-(* open Gstreamer *)
 
 module Opt = struct
   let verbose = ref false
+  let command = ref ""
 end
 
-let print_ = print_endline
-
-let filename_param =
-  let open Command.Param in
-  anon ("filename" %: string)
+module MyPipeline = struct
+  open Gstreamer
+  open Opt
+  let bin () = Gstreamer.Pipeline.parse_launch !command
+  let start_pipeline () =
+    init ();
+    (Element.set_state (bin ()) Element.State_playing : Element.state_change) |> ignore;
+    (Element.get_state (bin ()) : Element.state_change * Element.state * Element.state) |> ignore;
+    Unix.sleep 2;
+    (Element.set_state (bin ()) Element.State_null : Element.state_change) |> ignore;
+    deinit ();
+    Gc.full_major
+end
 
 let command =
   Command.basic
@@ -21,7 +29,8 @@ let command =
         and verbose = flag "-v" no_arg ~doc:"verbose output for debugging"
       in fun () ->
         if verbose then Opt.verbose := true;
-        print_ command
+        Opt.command := command;
+        MyPipeline.start_pipeline () ()
     )
 
 let () =
